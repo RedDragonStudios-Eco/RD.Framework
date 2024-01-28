49794be6a30fe7204c89752c313a667f5f68f7ae
ThelianTech <29005715+ThelianTech@users.noreply.github.com>
2024-01-27 17:59:13 -0700

    cmd

--- API/commands/Commands.cs
+++ API/commands/Commands.cs
@@ -9,20 +9,6 @@ namespace RD.Framework.API.commands
     {
         private static string RDVersion = "0.0.1";
 
-        [ChatCommand("Reloads the Unity Data Files without needing to reboot the server", "rl-unity", ChatAuthorizationLevel.Admin)]
-        public static void ReloadUnitlyData(IChatClient chatClient)
-        {
-            ModKitPlugin.ContentSync.RefreshContent();
-            chatClient.MsgLocStr("Unity Files Refreshed, Please Relog to get new changes.");
-        }
-
-        [ChatCommand("Rebuilds the ecopedia", "rl-ecopedia", ChatAuthorizationLevel.Admin)]
-        public static void ReBuildEcopedia(IChatClient chatClient)
-        {
-            EcopediaManager.Build(ModKitPlugin.ModDirectory);
-            chatClient.MsgLocStr("The Ecopedia has been Rebuilt and should auto update. Please check the console for any errors or issue with the rebuilt Ecopedia");
-        }
-
         [ChatCommand("Checks Verion of RD Framework", "rd-ver", ChatAuthorizationLevel.Admin)]
         public static void CheckModVersion(IChatClient chatClient)
         {

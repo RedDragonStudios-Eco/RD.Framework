15f2bf61376ae3d09124190cbbfd41fc5280fd70
ThelianTech <29005715+ThelianTech@users.noreply.github.com>
2024-02-03 07:21:53 -0700

    Added Temp Bucket recipe, Removed Duplicate Goats Milk Recipie.

---
+++ main/RDRanching/Recipies/BucketRecipie.cs
@@ -0,0 +1,65 @@
+﻿using Eco.Core.Items;
+using Eco.Gameplay.Components;
+using Eco.Gameplay.Items.Recipes;
+using Eco.Gameplay.Skills;
+using Eco.Mods.TechTree;
+using Eco.Shared.Localization;
+using RD.Framework.main.RDRanching.Items;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RD.Framework.main.RDRanching.Recipies
+{
+    [RequiresSkill(typeof(CarpentrySkill), 2)]
+    [Ecopedia("Items", "Products", subPageName: "Craft Bucket")]
+    public partial class CraftBucketRecipe : RecipeFamily
+    {
+        public CraftBucketRecipe()
+        {
+            var recipe = new Recipe();
+            recipe.Init(
+                name: "CraftBucket",  //noloc
+                displayName: Localizer.DoStr("Craft Bucket"),
+
+                // Defines the ingredients needed to craft this recipe. An ingredient items takes the following inputs
+                // type of the item, the amount of the item, the skill required, and the talent used.
+                ingredients: new List<IngredientElement>
+                {
+                    new IngredientElement(typeof(BoardItem), 2, true),
+                },
+
+                // Define our recipe output items.
+                // For every output item there needs to be one CraftingElement entry with the type of the final item and the amount
+                // to create.
+                items: new List<CraftingElement>
+                {
+                    new CraftingElement<BucketItem>(1)
+                });
+            this.Recipes = new List<Recipe> { recipe };
+            this.ExperienceOnCraft = 5; // Defines how much experience is gained when crafted.
+
+            // Defines the amount of labor required and the required skill to add labor
+            this.LaborInCalories = CreateLaborInCaloriesValue(70, typeof(CarpentrySkill));
+
+            // Defines our crafting time for the recipe
+            this.CraftMinutes = CreateCraftTimeValue(beneficiary: typeof(CraftBucketRecipe), start: 0.5f, skillType: typeof(CarpentrySkill));
+
+            // Perform pre/post initialization for user mods and initialize our recipe instance with the display name "BreedAngusCow"
+            this.ModsPreInitialize();
+            this.Initialize(displayText: Localizer.DoStr("Craft Bucket"), recipeType: typeof(CraftBucketRecipe));
+            this.ModsPostInitialize();
+
+            // Register our RecipeFamily instance with the crafting system so it can be crafted.
+            CraftingComponent.AddRecipe(tableType: typeof(CarpentryTableObject), recipe: this);
+        }
+
+        /// <summary>Hook for mods to customize RecipeFamily before initialization. You can change recipes, xp, labor, time here.</summary>
+        partial void ModsPreInitialize();
+
+        /// <summary>Hook for mods to customize RecipeFamily after initialization, but before registration. You can change skill requirements here.</summary>
+        partial void ModsPostInitialize();
+    }
+}

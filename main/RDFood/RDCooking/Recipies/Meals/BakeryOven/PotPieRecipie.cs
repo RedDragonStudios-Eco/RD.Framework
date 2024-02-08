f1de17e61adcc828a5f2b6b0e261518c1a958bcc
Shadowknight441 <78285802+Shadowknight441@users.noreply.github.com>
2024-02-07 19:42:51 -0500

    Update PotPieRecipie.cs

--- main/RDFood/RDCooking/Recipies/Meals/BakeryOven/PotPieRecipie.cs
+++ main/RDFood/RDCooking/Recipies/Meals/BakeryOven/PotPieRecipie.cs
@@ -15,7 +15,7 @@
     using RD.Framework.main.RDFood.RDCooking.Items.Meals.BakeryOven;
 
     [RequiresSkill(typeof(BakingSkill), 3)]
-    [Ecopedia("PotPie", "Baked Food", subPageName: "Pot Pie Item")]
+    [Ecopedia("Food", subPageName: "Pot Pie Item")]
     public partial class PotPieRecipe : RecipeFamily
     {
         public PotPieRecipe()

197cadb3308a9557bc85faee9e93cedee4bbda6c
ThelianTech <29005715+ThelianTech@users.noreply.github.com>
2024-02-10 10:40:46 -0700

    hotfix baby animal drop rates.

--- API/Helpers/RanchingBasePlugin.cs
+++ API/Helpers/RanchingBasePlugin.cs
@@ -31,31 +31,31 @@
 
             var bisonSpecies = typeof(Bison).GetField("species", bindings).GetValue(typeof(Bison)) as BisonSpecies;
             var list = bisonSpecies.GetType().GetProperty("ResourceList", bindings).GetValue(bisonSpecies) as List<SpeciesResource>;
-            list.Add(new SpeciesResource(typeof(BabyBisonItem), new Range(1, 1), 0.01f));
+            list.Add(new SpeciesResource(typeof(BabyBisonItem), new Range(0, 2), 0.1f));
 
             var mGoatSpecies = typeof(MountainGoat).GetField("species", bindings).GetValue(typeof(MountainGoat)) as MountainGoatSpecies;
             var mlist = mGoatSpecies.GetType().GetProperty("ResourceList", bindings).GetValue(mGoatSpecies) as List<SpeciesResource>;
-            mlist.Add(new SpeciesResource(typeof(BabyMountainGoatItem), new Range(1, 1), 0.01f));
+            mlist.Add(new SpeciesResource(typeof(BabyMountainGoatItem), new Range(0, 2), 0.1f));
 
             var bHornSpecies = typeof(BighornSheep).GetField("species", bindings).GetValue(typeof(BighornSheep)) as BighornSheepSpecies;
             var bHornList = bHornSpecies.GetType().GetProperty("ResourceList", bindings).GetValue(bHornSpecies) as List<SpeciesResource>;
-            bHornList.Add(new SpeciesResource(typeof(BabyBighornSheepItem), new Range(1, 1), 0.01f));
+            bHornList.Add(new SpeciesResource(typeof(BabyBighornSheepItem), new Range(0, 2), 0.1f));
 
             var turkeySpecies = typeof(Turkey).GetField("species", bindings).GetValue(typeof(Turkey)) as TurkeySpecies;
             var turkeyList = turkeySpecies.GetType().GetProperty("ResourceList", bindings).GetValue(turkeySpecies) as List<SpeciesResource>;
-            turkeyList.Add(new SpeciesResource(typeof(BabyTurkeyItem), new Range(1, 3), 0.1f));
+            turkeyList.Add(new SpeciesResource(typeof(BabyTurkeyItem), new Range(0, 3), 0.1f));
 
             var agoutiSpecies = typeof(Agouti).GetField("species", bindings).GetValue(typeof(Agouti)) as AgoutiSpecies;
             var agoutiList = agoutiSpecies.GetType().GetProperty("ResourceList", bindings).GetValue(agoutiSpecies) as List<SpeciesResource>;
-            agoutiList.Add(new SpeciesResource(typeof(BabyAgoutiItem), new Range(1, 3), 0.1f));
+            agoutiList.Add(new SpeciesResource(typeof(BabyAgoutiItem), new Range(0, 3), 0.1f));
 
             var hareSpecies = typeof(Hare).GetField("species", bindings).GetValue(typeof(Hare)) as HareSpecies;
             var hareList = hareSpecies.GetType().GetProperty("ResourceList", bindings).GetValue(hareSpecies) as List<SpeciesResource>;
-            hareList.Add(new SpeciesResource(typeof(BabyHareItem), new Range(1, 3), 0.1f));
+            hareList.Add(new SpeciesResource(typeof(BabyHareItem), new Range(0, 3), 0.1f));
 
             var prairieDogSpecies = typeof(PrairieDog).GetField("species", bindings).GetValue(typeof(PrairieDog)) as PrairieDogSpecies;
             var prairieDogList = prairieDogSpecies.GetType().GetProperty("ResourceList", bindings).GetValue(prairieDogSpecies) as List<SpeciesResource>;
-            prairieDogList.Add(new SpeciesResource(typeof(BabyPrairieDogItem), new Range(1, 3), 0.1f));
+            prairieDogList.Add(new SpeciesResource(typeof(BabyPrairieDogItem), new Range(0, 3), 0.1f));
 
 
             //ConsoleColors.PrintConsoleMultiColored("[EM Farming]", System.ConsoleColor.Magenta, " Added Baby Animals: Bison, Mountain Goat, Bighorn Sheep", System.ConsoleColor.White);

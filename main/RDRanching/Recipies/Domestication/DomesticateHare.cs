c9d34a2f4615bde94e0a55b8128bd91def61227d
ThelianTech <29005715+ThelianTech@users.noreply.github.com>
2024-02-20 21:18:55 -0700

    Added Hare Domestication Recipie.

---
+++ main/RDRanching/Recipies/Domestication/DomesticateHare.cs
@@ -0,0 +1,51 @@
+﻿namespace RD.Framework.main.RDRanching.Recipies.Domestication
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using System.ComponentModel;
+    using Eco.Core.Items;
+    using Eco.Gameplay.Components;
+    using Eco.Gameplay.Items.Recipes;
+    using Eco.Gameplay.Skills;
+    using Eco.Mods.TechTree;
+    using Eco.Shared.Localization;
+    using RD.Framework.main.RDRanching.Items;
+    using RD.Framework.main.RDRanching.Items.Animals.AdultAnimals;
+    using RD.Framework.main.RDRanching.Items.Animals.BabyAnimals;
+    using RD.Framework.main.RDRanching.Items.obj;
+
+    [RequiresSkill(typeof(HuntingSkill), 3)]
+    [Ecopedia("Items", "Animal", subPageName: "Rabbit")]
+    public partial class DomesticateHareRecipe : RecipeFamily
+    {
+        public DomesticateHareRecipe()
+        {
+            var recipe = new Recipe();
+            recipe.Init(
+                name: "DomesticateHare",
+                displayName: Localizer.DoStr("Domesticate Hare"),
+                ingredients: new List<IngredientElement>
+                {
+                    new IngredientElement(typeof(BabyHareItem), 1, true),
+                    new IngredientElement(typeof(AnimalFeedItem), 3, true),
+                },
+                items: new List<CraftingElement>
+                {
+                    new CraftingElement<RabbitItem>(1)
+                });
+            Recipes = new List<Recipe> { recipe };
+            ExperienceOnCraft = 5;
+            LaborInCalories = CreateLaborInCaloriesValue(70, typeof(HuntingSkill));
+            CraftMinutes = CreateCraftTimeValue(beneficiary: typeof(DomesticateHareRecipe), start: 5, skillType: typeof(HuntingSkill));
+            ModsPreInitialize();
+            Initialize(displayText: Localizer.DoStr("domesticate Hare"), recipeType: typeof(DomesticateHareRecipe));
+            ModsPostInitialize();
+            CraftingComponent.AddRecipe(tableType: typeof(NurseryObject), recipe: this);
+        }
+        partial void ModsPreInitialize();
+        partial void ModsPostInitialize();
+    }
+}
